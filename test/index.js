80b5d25489cf9049386c8b86eb9ddcd502f9e0fd
ninjascribble <sgrogan@weareratio.com>
2016-01-15 15:04:29 -0800

    First commit

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+
+describe('Life, the universe, and everything', () => {
+
+  it('answers to 42', () => {
+    assert.equal(42, 42);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var CopyPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [{
+
+  // Client-side JavaScript and CSS
+  context: path.join(__dirname, 'client'),
+  entry: './js/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    sourceMapFilename: '[file].map'
+  },
+  module: {
+    loaders: [{
+      test: /\.js/,
+      loader: 'babel'
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('style', ['css', 'sass?sourceMap'])
+    }, {
+      test: /\.gif|\.jpg|\.png/,
+      loader: 'file?name=[path][name].[ext]'
+    }, {
+      test: /\.woff|\.woff2|\.ttf|\.eot|\.svg/,
+      loader: 'file?name=[path][name].[ext]'
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css'),
+    new CopyPlugin([
+      { from: '../index.html', to: 'dist' }
+    ])
+  ]
+}];
